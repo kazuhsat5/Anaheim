1de6b737740f836b267c75ecac68bea6759a8fad
佐藤　和大 <kazuhsat@MBP-15JAC-671.local>
2015-08-21 15:26:38 +0900

    コントローラ修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,46 +5,36 @@ class TasksController < ApplicationController
 
     def new
         @task = Task.new
-        #
     end
 
     def edit
-        #
     end
 
     def create
-        # DBに保存
         @task = Task.new(task_params)
         @task.save
 
-        # トップページにリダイレクト
         redirect_to "/"
     end
 
     def update
-        # DBレコードの更新
         @task.update(task_params)
 
-        # トップページにリダイレクト
         redirect_to "/"
     end
 
     def dstroy
-        # DBレコードの削除
         @task.destroy
 
-        # トップページにリダイレクト
         redirect_to "/"
     end
 
     private
 
-        # パラメータで渡されたIDのタスクを取得する
         def set_task
             @task = Task.find(params[:id])
         end
 
-        # パラメータ(ハッシュ型)で渡されたタスク情報を取得する
         def task_params
             params.require(:task).permit(:priority, :item, :position, :findDate, :finder, :responder, :isCorrespond)
         end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,9 +3,9 @@ class TopController < ApplicationController
 
     def index
         # 未対応タスクの取得
-        @supported = Task.where.not(isCorrespond: 1)
+        @supported = Task.where.not(isCorrespond: 1).order(priority: :asc)
 
         # 対応済みタスクの取得
-        @unsupported = Task.where(isCorrespond: 1)
+        @unsupported = Task.where(isCorrespond: 1).order(priority: :asc)
     end
 end
